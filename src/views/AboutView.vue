e208463ef5d2e782cfbab7c6f3ffacb91387778d
ZYJiang <3144223912@qq.com>
2025-07-06 23:01:37 +0800

    新增开发者介绍页面

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+  
+    <el-space style="max-width: 100%;" wrap>
+      <ProfieldCard :avatar="wyx" name="王一昕"  bio="用户端页面设计" />
+      <ProfieldCard :avatar="cxz" name="陈炫茱"  bio="数据处理和后端UI设计" />
+      <ProfieldCard :avatar="ljj" name="刘俊佳"  bio="报告和PPT设计" />
+      <ProfieldCard :avatar="yax" name="余安心" bio="数据库设计" />
+      <ProfieldCard :avatar="crj" name="陈润均" bio="报告和PPT设计" />
+      <ProfieldCard :avatar="a" name="友人A" position="." bio="其他" />
+    </el-space>
+  
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+</template>
+<script lang='ts' setup>
+import ProfieldCard from '@/components/ProfileCard.vue'
+import crj from "@/assets/team/crj.jpg"
+import ljj from "@/assets/team/ljj.jpg"
+import wyx from "@/assets/team/wyx.jpg"
+import yax from "@/assets/team/yax.jpg"
+import cxz from "@/assets/team/cxz.jpg"
+import a from "@/assets/team/a.png"
+</script>
+<style lang="scss" scoped></style>
